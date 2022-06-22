7832088df1edb276c64c99f45f5aa8af0809973c
Ngaicode <mjombakevin376@gmail.com>
2022-06-22 14:43:19 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+// create a new PickupLocation with two properties
+function PickupLocation(address, city) {
+  this.address = address;
+  this.city = city;
+}
+// creates a new scooter with three properties
+function Scooter(year, color, model) {
+  this.year = year;
+  this.color = color;
+  this.model = model;
+}
+//create a new driver with three properties
+function Driver(name, age, experience) {
+  this.name = name;
+  this.age = age;
+  this.experience = experience;
+}
